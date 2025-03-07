aa43c93c9c60bf2e4a82f2c703c5039fa8847910
DimensionCoin <nick.vassallo97@gmail.com>
2025-03-07 05:36:38 -0500

    fix

--- components/shared/CompanyHeader.tsx
+++ components/shared/CompanyHeader.tsx
@@ -13,7 +13,7 @@ export function Header({  companyId }: HeaderProps) {
 
   return (
     <header className="bg-zinc-900 sticky top-0 z-10">
-      <div className="flex items-center justify-between h-10 px-2 py-1">
+      <div className="flex items-center justify-between  px-2 py-1">
         <div className="flex items-center gap-4">
           {/* Mobile Sidebar Trigger */}
           <Sheet>
@@ -21,7 +21,7 @@ export function Header({  companyId }: HeaderProps) {
               <Button
                 variant="outline"
                 size="icon"
-                className="md:hidden bg-zinc-900 border-zinc-800 mt-2"
+                className="md:hidden bg-zinc-900 border-zinc-800 "
               >
                 <Menu className="h-5 w-5" />
                 <span className="sr-only">Toggle navigation</span>
@@ -35,7 +35,7 @@ export function Header({  companyId }: HeaderProps) {
             </SheetContent>
           </Sheet>
         </div>
-        <div className="flex items-center justify-center mt-2">
+        <div className="flex items-center justify-center mt-1">
           <Link href={`/company/${companyId}/messages`}>
             <Button>
               <MessageCircle />
